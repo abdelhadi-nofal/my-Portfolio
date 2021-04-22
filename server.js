80c3f3323daf3ab43627453dcb491ce53532c245
abdelhadi-nofal <hadinofal1993@gmail.com>
2021-04-22 15:04:03 +0300

    env

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 
 const server = express();
 
-const PORT = process.nextTick.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 
 server.use(express.static('./public'));
